e7361c83b69812bf874fee3f28df09b23093160d
darfux <darfux@163.com>
2014-02-09 01:02:49 +0800

    mark

--- main.rb
+++ main.rb
@@ -6,47 +6,95 @@ require 'hpricot' #html analyzer
 require 'net/http'
 require 'yaml'
 class Frigga
+  :READY
+  :SUCCESS
+  :FAIL
+  :CAPTCHA_WRONG
+  :USER_INFO_WRONG
+  def initialize(doretry=true)
+    @cookie = ""
+    @captcha = ""
+    @user = {id:nil, pwd:nil}
+    @retry = doretry
+    @retrycounter = 8
+    @status = {stat: :READY, info:nil}
+  end
+
+  def login(id, pwd)
+    @user[:id] = id
+    @user[:pwd] = pwd
+
+    get_cookie
+
+    loop do
+      get_captha_text
+      uri = URI(LOGIN_URL)  
+      http = Net::HTTP.start(uri.host, uri.port)
+      data = gen_form_data({
+        operation:        FORM_operation,
+        usercode_text:    id+'1',
+        userpwd_text:     pwd,
+        checkcode_text:   @captcha,
+        submittype:       FORM_submittype
+      })
+      headers = {'Cookie' => @cookie}
+
+      resp = http.post(uri.request_uri, data, headers)
+      body = Frigga::get_utf8_body(resp)
+      doc = Hpricot(body)
+      fail = doc.search('noframes').first.nil?
+      if fail
+        info = doc.search('li').first
+        if info.nil?
+          raise 'Unknown wrong.'
+        end
+
+        content = info.inner_html
+        if content.match("请输入正确的验证码！")
+          @status[:stat] = :FAIL
+          @status[:info] = :CAPTCHA_WRONG
+          break unless @retry && @retrycounter>0
+          @retrycounter-=1
+          puts 'ocr failed,retrying...'
+          sleep 1
+          redo
+        end
+        if content.match("用户不存在或密码错误！")
+          @status[:stat] = :FAIL
+          @status[:info] = :USER_INFO_WRONG
+          break
+        end
+
+        raise 'Unknown wrong.'
+      end
+      @status[:stat] = :SUCCESS
+      @status[:info] = nil
+    end
+    # info = doc.search('li').first
+    # if info && info.inner_html.match("请输入正确的验证码！")
+    #   p 'e'
+    # end
+  end
+
+  def to_s
+    org = super
+    org.chop<<"\n[user]#{@user}; \n[cookie]#{@cookie}"<<'>'
+  end
+  
+protected
   CAPTCHA_FILE = "captcha.jpg"
   URL = 'http://222.30.32.10/'
   VC_URL = URL+'ValidateCode' # CAPTCHA_URL
   LOGIN_URL = URL+'stdloginAction.do'
 
-  FORM_operation = 'no'.freeze
-  FORM_submittype = '%C8%B7+%C8%CF'.freeze
-  
+  FORM_operation = 'no'
+  FORM_submittype = '%C8%B7+%C8%CF'
   def self.get_utf8_body(resp)
     body = resp.body
     doc = Hpricot(body)
     encoding = doc.search("meta[@content]").first.attributes['content'].split('charset=')[1]
     body.force_encoding(encoding).encode("UTF-8")
   end
-  
-  def initialize
-    @cookie = ""
-    @captcha = ""
-  end
-
-  def login(id, pwd)
-    get_cookie
-
-    get_captha_text
-
-    uri = URI(LOGIN_URL)  
-    http = Net::HTTP.start(uri.host, uri.port)
-    data = gen_form_data({
-      operation:        FORM_operation,
-      usercode_text:    id,
-      userpwd_text:     pwd,
-      checkcode_text:   @captcha,
-      submittype:       FORM_submittype
-    })
-    headers = {'Cookie' => @cookie}
-
-    #response
-    http.post(uri.request_uri, data, headers)
-  end
-
-protected
 
   def gen_form_data(list)
     data = ""
@@ -76,7 +124,6 @@ protected
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-
     @cookie = response.get_fields('set-cookie')[0].split(';')[0]
   end
 end
@@ -91,6 +138,7 @@ f = File.open('user.yml','r')
 data = f.read
 user = YAML.load(data)
 
-resp = Frigga.new.login(user[:id], user[:pwd])
+fr = Frigga.new
+body = fr.login(user[:id], user[:pwd])
 
-print Frigga::get_utf8_body resp
+# print body
