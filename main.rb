1e3d5a53bcfe3e0666c08666641e153ce6c7dd5e
darfux <darfux@163.com>
2014-02-08 13:04:51 +0800

    form data bug fixed

--- main.rb
+++ main.rb
@@ -10,9 +10,12 @@ URL = 'http://222.30.32.10/'
 VC_URL = URL+'ValidateCode' # CAPTCHA_URL
 LOGIN_URL = URL+'stdloginAction.do'
 
-# user.yml
-#   id: 111XXXX
-#   pwd: ********
+=begin
+user.yml
+---
+:id: 'yourid'
+:pwd: yourpassword
+=end
 f = File.open('user.yml','r')
 data = f.read
 user = YAML.load(data)
@@ -40,34 +43,42 @@ img.close
 img = MiniMagick::Image.new(CAPTCHA_FILE)
 img.resize(200)
 
-# checkcode = RTesseract.new(img.path).to_s_without_spaces
-checkcode = gets
+checkcode = RTesseract.new(img.path).to_s_without_spaces
 
-uri = URI.parse(LOGIN_URL)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri)
-request.set_form_data({
-                        operation:'no',
-                        usercode_text: usercode,
-                        userpwd_text: userpwd,
-                        checkcode_text: checkcode,
-                        submittype:'%C8%B7+%C8%CF'
-                      })
-request.initialize_http_header({ "Cookie" => cookie })
-response = http.request(request)
+uri = URI(LOGIN_URL)  
+http = Net::HTTP.start(uri.host, uri.port)
+data = 
+"operation=no&"<<
+"usercode_text=#{usercode}&"<<
+"userpwd_text=#{userpwd}&"<<
+"checkcode_text=#{checkcode}&"<<
+"submittype=%C8%B7+%C8%CF"
+headers = {
+  'Cookie' => cookie,
+  'Referer' => URL,
+  'Content-Type' => 'application/x-www-form-urlencoded'
+}
 
-body = response.body
-
-# 从body中找到charset="XXX"
-charset_begin = body.index("charset=")+9
-charset_end = body.index("\"", charset_begin)
-body_encoding = body[charset_begin, charset_end-charset_begin]
-
-body = response.body.force_encoding(body_encoding).encode("UTF-8")
-
-if body.empty?
-  puts "success"
-else
-  puts "wrong#{checkcode}"
-  exit
+def get_utf8_body(resp)
+  body = resp.body
+  charset_begin = body.index("charset=")+8
+  charset_end = body.index("\"", charset_begin)
+  encoding = body[charset_begin, charset_end-charset_begin]
+  resp.body.force_encoding(encoding).encode("UTF-8")
 end
+resp = http.post(uri.request_uri, data, headers)
+
+body = get_utf8_body resp
+
+print body
+
+# get encoding from html's charset
+
+# body = response.body.force_encoding(body_encoding).encode("UTF-8")
+
+# if body.empty?
+#   puts "success"
+# else
+#   puts "wrong#{checkcode}"
+#   exit
+# end
