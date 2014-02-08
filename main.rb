eab006c32fd4caa702f29fe3a344ba1d3e54f96f
darfux <darfux@163.com>
2014-02-08 15:27:45 +0800

    doing encapsulation

--- main.rb
+++ main.rb
@@ -11,6 +11,66 @@ URL = 'http://222.30.32.10/'
 VC_URL = URL+'ValidateCode' # CAPTCHA_URL
 LOGIN_URL = URL+'stdloginAction.do'
 
+FORM_operation = 'no'.freeze
+FORM_submittype = '%C8%B7+%C8%CF'.freeze
+# get encoding from html's charset
+def get_utf8_body(resp)
+  body = resp.body
+  doc = Hpricot(body)
+  encoding = doc.search("meta[@content]").first.attributes['content'].split('charset=')[1]
+  body.force_encoding(encoding).encode("UTF-8")
+end
+
+def gen_form_data(list)
+  data = ""
+  list.each do |key, val|
+    data<<"#{key}=#{val}"<<'&'
+  end
+  data.chop
+end
+
+def get_captha_text(cookie)
+  uri = URI.parse(VC_URL)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.initialize_http_header({ "Cookie" => cookie })
+  response = http.request(request)
+  img = File.open(CAPTCHA_FILE, 'w')
+  img.write(response.read_body)
+  img.close
+  img = MiniMagick::Image.new(CAPTCHA_FILE)
+  img.resize(200)
+
+  #OCR result
+  RTesseract.new(img.path).to_s_without_spaces
+end
+
+def get_cookie
+  uri = URI.parse(URL)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+
+  #cookie
+  response.get_fields('set-cookie')[0].split(';')[0]
+end
+
+def login(id, pwd, checkcode, cookie)
+  uri = URI(LOGIN_URL)  
+  http = Net::HTTP.start(uri.host, uri.port)
+  data = gen_form_data({
+    operation:        FORM_operation,
+    usercode_text:    id,
+    userpwd_text:     pwd,
+    checkcode_text:   checkcode,
+    submittype:       FORM_submittype
+  })
+  headers = {'Cookie' => cookie}
+
+  #response
+  http.post(uri.request_uri, data, headers)
+end
+
 =begin
 user.yml
 ---
@@ -21,54 +81,9 @@ f = File.open('user.yml','r')
 data = f.read
 user = YAML.load(data)
 
-usercode = user[:id]
-userpwd = user[:pwd]
+cookie = get_cookie
 
-# login
-uri = URI.parse(URL)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-response = http.request(request)
+checkcode = get_captha_text(cookie)
 
-cookie = response.get_fields('set-cookie')[0].split(';')[0]
-
-# get CAPTCHA
-uri = URI.parse(VC_URL)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-request.initialize_http_header({ "Cookie" => cookie })
-response = http.request(request)
-img = File.open(CAPTCHA_FILE, 'w')
-img.write(response.read_body)
-img.close
-img = MiniMagick::Image.new(CAPTCHA_FILE)
-img.resize(200)
-
-checkcode = RTesseract.new(img.path).to_s_without_spaces
-
-uri = URI(LOGIN_URL)  
-http = Net::HTTP.start(uri.host, uri.port)
-data = 
-"operation=no&"<<
-"usercode_text=#{usercode}&"<<
-"userpwd_text=#{userpwd}&"<<
-"checkcode_text=#{checkcode}&"<<
-"submittype=%C8%B7+%C8%CF"
-headers = {
-  'Cookie' => cookie,
-  'Referer' => URL,
-  'Content-Type' => 'application/x-www-form-urlencoded'
-}
-
-# get encoding from html's charset
-def get_utf8_body(resp)
-  body = resp.body
-  doc = Hpricot(body)
-  encoding = doc.search("meta[@content]").first.attributes['content'].split('charset=')[1]
-  body.force_encoding(encoding).encode("UTF-8")
-end
-resp = http.post(uri.request_uri, data, headers)
-
-body = get_utf8_body(resp)
-
-print body
+resp = login(user[:id], user[:pwd], checkcode, cookie)
+print get_utf8_body(resp)
